cef42e16b2aeef9fc484bcea152b9de58ab79914
Sid <siddhata.s.patil@gmail.com>
2016-07-05 13:56:34 -0700

    Create random_generator_range.cpp

---
+++ random_generator_range.cpp
@@ -0,0 +1,31 @@
+/*
+ ==============================================================================================================================
+ Name        : random_generator_range.cpp
+ Author      : Siddhata Patil
+ Copyright   : Copyright (c) Siddhata Patil. Sourcecode rights reserved.
+ Copyright   : Copyright (c) Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
+ Question    : Print random nos in a particular range
+ ==============================================================================================================================
+ */
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include <cstdlib>
+#include <ctime>
+
+int randRange(int low, int high)
+{	
+	return rand()%(high-low) + low;
+}
+
+int main()
+{
+	srand (time(NULL));
+	for(int i = 0; i<25; i++)
+	{
+		cout << randRange(7,25)<<endl;
+	}
+system ("pause");
+return 0;
+}
