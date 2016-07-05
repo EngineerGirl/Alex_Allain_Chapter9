8a79e724b227a9007f54b5c28bf761838c9a6a70
Sid <siddhata.s.patil@gmail.com>
2016-07-05 13:52:50 -0700

    Create random_generator_skeleton.cpp

---
+++ random_generator_skeleton.cpp
@@ -0,0 +1,31 @@
+/*
+ ==============================================================================================================================
+ Name        : random_generator_skeleton.cpp
+ Author      : Siddhata Patil
+ Copyright   : Copyright (c) Siddhata Patil. Sourcecode rights reserved.
+ Copyright   : Copyright (c) Alex_Allain_Jumping_into_Cpp_Book. Question rights reserved.
+ Question    : Write a menu program that lets the user select from a list of options, and if the input is not one
+               of the options, reprint the list
+ ==============================================================================================================================
+ */
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+//	srand (time(NULL));
+int seed = time (NULL);
+cout<<"Random generator seed: "<<seed<<endl;
+srand (seed);
+
+	for(int i = 0; i<10; i++)
+	{
+		cout<< ((rand()%11)+1)<<endl;
+	}
+system ("pause");
+return 0;
+}
